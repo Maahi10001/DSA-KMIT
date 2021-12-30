47c34ede1a884bedbea3368f1bb41b8d84dc8964
THANDA MAHENDER GOUD <80961448+Maahi10001@users.noreply.github.com>
2021-12-30 20:13:10 +0530

    DAY-13

---
+++ DAY-13/A/Day-13-P-2.py
@@ -0,0 +1,71 @@
+'''
+There are B bags containing N gold boxes each. In each bag, gold boxes are 
+arranged  in ascending order of their weights strictly, create a method in 
+such a way that we need to return the least weight of gold box which is 
+common least weight in all the given bags.
+
+If we don’t have any common least weighted gold box, among all the bags then return -1.
+
+Input Format:
+-------------
+Line-1: Two integers B and N, number of bags and number of goldboxes in each Bag.
+Next B lines: N space separated integers, weights of GoldBoxes.
+
+Output Format:
+--------------
+Print the least weight of gold box, if found
+Print -1, if not found.
+
+
+Sample Input:
+---------------
+5 5
+1 2 3 4 5
+2 3 6 7 9
+1 2 3 5 8
+1 3 4 6 8 
+2 3 5 7 8
+
+Sample Output:
+----------------
+3
+'''
+#solution
+import math, sys
+def binarySearch(l, ele):
+    
+    s = 0
+    e = len(l)-1
+    
+    while s <= e:
+        
+        mid = math.ceil((s+e)/2)
+        
+        if l[mid] == ele:
+            return True
+        
+        if l[mid] > ele:
+            e = mid-1
+            
+        if l[mid] < ele:
+            s = mid+1
+            
+    return False
+
+b, n = map(int, input().split())
+L = []
+for i in range(b):
+    L.append(list(map(int, input().split())))
+
+j = 1
+for i in L[0]:
+    while j < b:
+        if not binarySearch(L[j], i):
+            break
+        j += 1
+    if j == b:
+        print(i)
+        exit(0)
+    else:
+        j = 1
+print(-1)
---
+++ DAY-13/A/Day-13-P-3.py
@@ -0,0 +1,57 @@
+'''
+In a shopping mall, there is a Lift with a capacity of 500kgs only.
+There are N persons waiting for the lift, and their weights (weights[]) are given.
+If The Lift is overloaded, it will not move.
+ 
+Your task is to find out the maximum number of persons can use the Lift,
+without any overloading issue.
+
+Input Format:
+-------------
+Line-1: An integer N, number of persons
+Line-2: N space separated integers, weights of the persons.
+
+Output Format:
+--------------
+Print an integer, max num of persons canuse the lift.
+
+
+Sample Input-1:
+---------------
+6
+98 121 76 135 142 65
+
+Sample Output-1:
+----------------
+5
+
+
+Sample Input-2:
+---------------
+7
+85 67 69 83 54 61 50
+
+Sample Output-2:
+----------------
+7
+
+
+'''
+
+
+#solution
+def func(l):
+    val=0
+    c=0
+    l.sort()
+    if sum(l)<=500:
+        return len(l)
+    else:
+        for i in range(n):
+            val=val+l[i]
+            if(val>500):
+                return i
+               
+n=int(input())
+l=list(map(int,input().split()))[:n]
+print(func(l))
